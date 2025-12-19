56983552853469971978ea9b24841d413e43ec5e
Neelansh Panday <neelanshpanday@gmail.com>
2025-12-20 00:42:27 +0530

    backendset

--- parent/provider.tf
+++ parent/provider.tf
@@ -5,6 +5,15 @@ terraform {
       version = "4.56.0"
     }
   }
+
+   backend "azurerm" {
+    subscription_id = "c060d854-01c4-4739-baf7-57540765c3d2"
+    resource_group_name = "rg_practice"
+    storage_account_name = "storageassig1912"
+    container_name = "blobcon"
+    key = "neelterraform.tfstate"
+    
+  }
 }
 
 provider "azurerm" {
